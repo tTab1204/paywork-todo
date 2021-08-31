59dd5916c1b9ac6048ec069b4882d896d53f599b
JuYoung <ken44929@gmail.com>
2021-08-31 13:02:20 +0900

    Feat: #1 Header 컴포넌트 생성

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { LeftBox, NavbarContainer, NavbarWrapper, RightBox } from './style';
+import { HomeOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { showModal } from 'store/actions/modals_action';
+import { RootState } from 'store/reducers';
+import Modal from 'components/Modal/Modal';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const modal = useSelector((state: RootState) => state.modal);
+
+  const onHandleModal = () => {
+    dispatch(showModal());
+  };
+
+  return (
+    <NavbarContainer>
+      <NavbarWrapper>
+        <LeftBox>
+          <HomeOutlined />
+        </LeftBox>
+        <RightBox>
+          <button onClick={onHandleModal}>할 일 추가</button>
+        </RightBox>
+      </NavbarWrapper>
+      {modal.showModal && <Modal />}
+    </NavbarContainer>
+  );
+};
+
+export default Header;
